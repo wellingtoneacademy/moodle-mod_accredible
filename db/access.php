29e85de0e18fa473419833ded578742a412650c8
Ryan Booth <ryanbooth77@gmail.com>
2014-10-24 18:03:33 +0530

    removed student access

--- db/access.php
+++ db/access.php
@@ -42,7 +42,6 @@ $capabilities = array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
         'archetypes' => array(
-            'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
