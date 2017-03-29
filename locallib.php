c0635fe76b01787c7e542e08e54f3a65402c755c
Alan Heppenstall <alanheppenstall@Alans-MacBook-Pro.local>
2017-03-29 11:29:44 -0700

    Update get_issued to use all_credentials endpoint

--- locallib.php
+++ locallib.php
@@ -219,6 +219,29 @@ function accredible_get_issued($achievement_id) {
 }
 
 /**
+ * List all of the ceritificates with a specific achievement id & email
+ *
+ * @param string $achievement_id
+ * @return array[stdClass] $certificates
+ */
+function accredible_get_issued($achievement_id, $email) {
+	global $CFG;
+
+	$curl = curl_init('https://api.accredible.com/v1/all_credentials?group_id='.urlencode($achievement_id).'&email='.$email);
+	curl_setopt( $curl, CURLOPT_HTTPHEADER, array( 'Authorization: Token token="'.$CFG->accredible_api_key.'"' ) );
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	if(!$result = json_decode( curl_exec($curl) )) {
+	  // throw API exception
+	  // include the achievement id that triggered the error
+	  // direct the user to accredible's support
+	  // dump the achievement id to debug_info
+	  throw new moodle_exception('getissuederror', 'accredible', 'https://accredible.com/contact/support', $achievement_id, $achievement_id);
+	}
+	curl_close($curl);
+	return $result->credentials;
+}
+
+/**
  * List all of the issuer's templates
  *
  * @return array[stdClass] $templates
@@ -611,7 +634,7 @@ function accredible_post_evidence($credential_id, $evidence_item, $allow_excepti
 function accredible_check_for_existing_certificate($achievement_id, $user) {
 	global $DB;
 	$existing_certificate = false;
-	$certificates = accredible_get_issued($achievement_id);
+	$certificates = accredible_get_issued($achievement_id, $user->email);
 
 	foreach ($certificates as $certificate) {
 		if($certificate->recipient->email == $user->email) {
