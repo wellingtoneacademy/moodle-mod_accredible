b82332e652d1abfce99b6fec22467abe07dae65d
Matthew Terentjevs <matthew.terentjevs@wellingtone.co.uk>
2020-08-14 15:19:47 +0100

    Stop listenning for quiz completion handler

--- db/events.php
+++ db/events.php
@@ -30,12 +30,12 @@ defined('MOODLE_INTERNAL') || die();
 
 $observers = array(
     // Listen for finished quizes.
-    array(
+/*    array(
         'eventname' => '\mod_quiz\event\attempt_submitted',
         'includefile' => '/mod/accredible/locallib.php',
         'callback' => 'accredible_quiz_submission_handler',
         'internal' => false
-    ),
+    ),*/
     // Course completed only runs with a cron job. There's no other way to ensure course completion without the Moodle course completion cron job running.
      array(
         'eventname'   => '\core\event\course_completed',
